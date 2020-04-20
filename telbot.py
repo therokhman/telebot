d8d66cdb3ebb30e32d544a0cf1f2da429d21636f
alex_rokhman <alex.bobr@rambler.ru>
2020-04-20 17:41:10 +0300

    create conf.py

--- telbot.py
+++ telbot.py
@@ -1,36 +1,34 @@
 from telebot import types, TeleBot
-import configparser
+from config import *
 
-config = configparser.ConfigParser()
-config.read('config.ini')
 
-bot = TeleBot(config['DEFAULT']['Token'])
+bot = TeleBot(Token)
 
 
 keyboard1 = types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Пока')
+keyboard1.row("Привет", "Пока")
 keyboard2 = types.ReplyKeyboardMarkup(True, True)
-keyboard2.row('Как дела', 'Пока')
+keyboard2.row("Как дела", "Пока")
 
 
-@bot.message_handler(commands=['start', 'go'])
+@bot.message_handler(commands=["start", "go"])
 def start_message(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, 'Ну что, начнем работу', reply_markup=keyboard1)
+    bot.send_message(chat_id, "Ну что, начнем работу", reply_markup=keyboard1)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=["text"])
 def text_handler(message):
     text = message.text.lower()
     chat_id = message.chat.id
     user_name = message.from_user.first_name
     if text == "привет":
-        bot.send_message(chat_id, f'Привет, {user_name}', reply_markup=keyboard2)
+        bot.send_message(chat_id, f"Привет, {user_name}", reply_markup=keyboard2)
     elif text == "как дела":
-        bot.send_message(chat_id, 'норм')
+        bot.send_message(chat_id, "норм")
     else:
-        bot.send_message(chat_id, 'пока')
+        bot.send_message(chat_id, "пока")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.polling(none_stop=True)
